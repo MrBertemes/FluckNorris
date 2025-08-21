7ac3784361bdf9053a2da8e257be9663a5bfbc70
Brian Laus Bertemes <bertemesbrian@gmail.com>
2025-08-21 13:03:41 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/flucknorris/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flucknorris
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/model/jokes_respository.dart
@@ -0,0 +1,7 @@
+class JokesRespository {
+  final String url =  "https://api.chucknorris.io/jokes";
+
+  Future<List<String>> getCategories () async {
+    
+  }
+}
